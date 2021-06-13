6ec811f19127b7c8caef7d6b7b0e08e95bb3e1f9
sean <sean@gomicorp.com>
2021-06-14 01:26:30 +0900

    [feat] HttpErrorFilter 추가
    모든 exception을 공통 포멧으로 처리하기 위해 HttpErrorFilter 추가.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
   imports: [
@@ -17,6 +19,12 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
   ],
 })
 export class AppModule {}
