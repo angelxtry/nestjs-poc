c590bafcb412994cb3ed93acdbc6acd9394aa825
sean <sean@gomicorp.com>
2021-06-13 23:07:15 +0900

    [feat] signup 로직 추가
    auth-credentials.dto.ts 생성.
    dto에 validation 추가.
    repository에 createUser 로직 추가.
    service, controller 추가.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,7 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private readonly userRepository: UsersRepository,
   ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
 }
