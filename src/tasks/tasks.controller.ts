2377f64c8e5e932114af3153cb090db5278cad10
sean <sean@gomicorp.com>
2021-06-14 22:12:53 +0900

    [feat] auth custom decorator 생성
    createParamDecorator를 이용하여 GetUser 데코레이터를 생성.
    TaskModule imports에 AuthModule 추가.
    TaskController class에 `@UseGuards(AuthGuard())`를 사용하여 TaskController 전체를 인증에 포함시킬 수 있다.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,14 +8,17 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksService } from './tasks.service';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { FilterDto } from './dto/filter.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   private logger = new Logger('TasksController');
   constructor(private taskService: TasksService) {}
