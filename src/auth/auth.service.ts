fea4dba3fd45d130590cbcccad2aa4f79669dd39
sean <sean@gomicorp.com>
2021-06-14 21:08:43 +0900

    [feat] jwt 적용
    @nestjs/jwt @nestjs/passport passport passport-jwt 설치.
    AuthModule imports에 PassportModule, JwtModule 추가.
    AuthService constructor에 JwtService 추가.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,21 +3,30 @@ import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interface/jwt-payload.interface';
+import { AccessToken } from './interface/access-token.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private readonly userRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<AccessToken> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ username });
-    if (user && (await compare(password, user.password))) return 'success';
-    else throw new UnauthorizedException('Please check user credentials.');
+    if (user && (await compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check user credentials.');
+    }
   }
 }
