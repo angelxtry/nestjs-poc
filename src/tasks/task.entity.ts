267c925a9805c6ceb6e412cefce86ab4d8516e1a
sean <sean@gomicorp.com>
2021-06-17 21:54:56 +0900

    [feat] user와 task에 realtion 추가
    user vs task -> 1:N 구조
    task 테이블에 ManyToOne, user 테이블에 OneToMany 추가.
    task 테이블에 user_id 컬럼이 생성됨.

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Task {
@@ -15,4 +16,8 @@ export class Task {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 }
